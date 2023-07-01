a244367b1170d5aa94638e2628a822c112165637
Codemyhobby <alhassandamilola16@gmail.com>
2023-07-01 02:06:06 +0100

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import profileImg from '../../assets/profile-img.png';
+import profileImg from '../../assets/profile-pic.png';
 import shapeOne from '../../assets/shape-1.png';
 import shapeTwo from '../../assets/shape-2.png';
 import CV from "../../assets/CV.pdf"
