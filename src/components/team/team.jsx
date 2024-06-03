6e0ae7bf89ffc5156f80c08f75044ebd6197a32f
Codemyhobby <alhassandamilola16@gmail.com>
2024-06-03 17:58:10 +0100

    team

---
+++ src/components/team/team.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const team = () => {
+  return (
+    <section className="section team" id="rteam">
+        <div className="container team__container grid">
+            <div className="tean-details">
+                <div className="teamn-img-container">
+
+                </div>
+                <div className="team-member-title">
+                    
+                </div>
+            </div>
+            <div className="tean-details"></div>
+            <div className="tean-details"></div>
+        </div>
+    </section>
+  )
+}
+
+export default team
