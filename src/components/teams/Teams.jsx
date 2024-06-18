380de4c7307cdbe4575007876cc7953b56babf1f
Codemyhobby <alhassandamilola16@gmail.com>
2024-06-18 14:10:29 +0100

    team members

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Team2 from '../../assets/team-2.jpg';
+import Team2 from '../../assets/team-5.jpg';
 import Team3 from '../../assets/team-3.jpg';
 import Team4 from '../../assets/team-4.jpg';
+import Team5 from '../../assets/team-6.jpg';
 import Team1 from '../../assets/team-01.jpg'
 import shapeOne from '../../assets/shape-1.png';
 import './teams.css';
@@ -43,6 +44,17 @@ const Teams = () => {
                     <h4>Abdullah</h4>
                 </div>
             </div>
+
+            <div className="team-member card">
+                <div className="team-member-img">
+                    <img src={Team5} alt="" />
+                </div>
+                <div className="team-member-name">
+                    <h3>UI/UX Designer</h3>
+                    <h4>Md. Al Amin</h4>
+                </div>
+            </div>
+
             <div className="team-member card">
                 <div className="team-member-img">
                     <img src={Team4} alt="" />
